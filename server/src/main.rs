2ef14eb8ce6b2cdf0739919780992a8ddf3afa4e
Glazzerino <fztmon@gmail.com>
2019-08-28 11:43:01 -0700

    Error and Ok cases

--- server/src/main.rs
+++ server/src/main.rs
@@ -14,10 +14,17 @@ fn main() {
 fn handle_connection(mut stream: &TcpStream) {
     let mut buffer = [0;512];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}",
-    String::from_utf8_lossy(&buffer[..]));
-    
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}",contents);
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+     
+    } else {
+        ("HTTP/1.1 400 NOT FOUND\r\n\r\n", "404.html")
+       
+    };
+    let file_contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}",status_line,file_contents);
     stream.write(response.as_bytes()).unwrap();
+    println!("SENT A RESPONSE");
+    stream.flush().unwrap();
 }
